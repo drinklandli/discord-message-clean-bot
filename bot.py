a4a2437354662d0a18dccc8f36cf51a83d4e5d04
Salih Drinkoğlu <93040162+drinklandli@users.noreply.github.com>
2022-11-10 23:58:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,9 +5,9 @@ bot = discord.Client(command_prefix="")
 
 @bot.event
 async def on_message(message):
-    if message.content.lower().startswith("purge"):
-        arr=message.content.split(" ")
-        amount = int(arr[0])
-        await message.channel.purge(limit=amount)
-        await message.channel.send(str(amount)+" "+"messages have deleted successfully")
+    if message.content.lower().startswith("siler misin"):
+        liste=message.content.split(" ")
+        sayi = int(liste[0])
+        await message.channel.purge(limit=sayi)
+        await message.channel.send(str(sayi)+" "+"mesaj başarıyla silindi.")
 bot.run('token')
